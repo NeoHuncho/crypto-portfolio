7a6f4ddd836564f5294501ace447da11a6a737b0
Neo Huncho <william.guinaudie@gmail.com>
2022-08-09 13:29:02 +0200

    debugging env errors

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -21,6 +21,12 @@ const fuego = new Fuego(firebaseConfig);
 initializeApp(firebaseConfig);
 
 function MyApp({ Component, pageProps }) {
+  console.log({
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  });
   return (
     <MantineProvider
       theme={{ colorScheme: "dark" }}
