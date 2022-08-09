9d11a29f1bd4e003cb2e2c31341c4e51d475c7cf
Neo Huncho <william.guinaudie@gmail.com>
2022-08-09 14:56:43 +0200

    debugging env errors

---
+++ frontend/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
+};
