a447bcfe8dc03ae008c9fe11b7dff3fd2f04bc7d
Neo Huncho <william.guinaudie@gmail.com>
2022-09-14 11:51:53 +0200

    fixed backend function

--- backend/src/initFireBase.ts
+++ backend/src/initFireBase.ts
@@ -17,7 +17,7 @@ const initFirebase = async () => {
 };
 
 const private_key = process.env["FIREBASE_PRIVATE_KEY"] || "";
-
+console.log(1, private_key)
 const { privateKey } = JSON.parse(private_key);
 
 const service_account: any = {
