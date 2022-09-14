3441eb2263455ebd8fa131590c254e9e740cd846
Neo Huncho <william.guinaudie@gmail.com>
2022-09-14 12:05:57 +0200

    fixed backend function

--- backend/src/functions/checkUpdateCardHistory.ts
+++ backend/src/functions/checkUpdateCardHistory.ts
@@ -2,11 +2,11 @@ import { readdir, unlink, rename } from "fs/promises";
 import XLSX from "xlsx";
 
 const checkAndUpdateCardHistory = async () => {
-  const userImports = await readdir("../imports");
+  const userImports = await readdir("/backend/src/imports");
 
   if (!userImports) return;
   for (const file in userImports) {
-    let workbook = XLSX.readFile("../imports/" + userImports[file]);
+    let workbook = XLSX.readFile("/backend/src/imports" + userImports[file]);
     let sheet = workbook.SheetNames;
     if (sheet.length === 0) return;
     let data: any = XLSX.utils.sheet_to_json(workbook.Sheets[sheet[0]])[0];
@@ -18,8 +18,8 @@ const checkAndUpdateCardHistory = async () => {
         }
 
         await rename(
-          "imports/" + userImports[file],
-          "imports/CardHistoryFinal.xlsx"
+          "/backend/src/imports/" + userImports[file],
+          "/backend/src/imports/CardHistoryFinal.xlsx"
         );
       }
   }
